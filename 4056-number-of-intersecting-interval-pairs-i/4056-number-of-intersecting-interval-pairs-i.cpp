aebc553254be509367711084bc2dd51e68295239
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2026-09-21 19:02:41 +0900

    Time: 0 ms (100%), Space: 36.4 MB (50%) - LeetHub

---
+++ 4056-number-of-intersecting-interval-pairs-i/4056-number-of-intersecting-interval-pairs-i.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countIntersectingIntervals(vector<vector<int>>& intervals) {
+        int count = 0;
+        for (int i = 0; i < intervals.size() - 1; i++) {
+            for (int j = i + 1; j < intervals.size(); j++) {
+                if (intervals[i][1] >= intervals[j][0] && intervals[j][1] >= intervals[i][0]) {
+                    ++count;
+                }
+            }
+        }
+        return count;
+    }
+};
